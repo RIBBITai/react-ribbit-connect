1f9a6cf92c909f7932f689ef38c85bc7c3d64d75
Darryl Huffman <dyudabomb@gmail.com>
2022-10-27 10:27:55 -0400

    added build to dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.jsx',
     output: {
-    filename: 'ribbit-connect.min.js',
+    filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
